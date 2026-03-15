de4431e23c06554d46b50d1f12085c635bf0c2a9
philipfrese <philip.frese@stirnberg.net>
2026-03-15 20:51:17 +0100

    chore: basic project setup

---
+++ prettier.config.ts
@@ -0,0 +1,14 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  endOfLine: "lf",
+}
+
+export default config;
