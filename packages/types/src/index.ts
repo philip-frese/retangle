237bfc0ad0eb42f67042908ae0f110b99f5fe12b
philipfrese <philip.frese@stirnberg.net>
2026-03-29 18:12:40 +0200

    feat: create types package

---
+++ packages/types/src/index.ts
@@ -0,0 +1,41 @@
+export type HookDefinition = {
+  name: string;
+  filePath: string;
+  dependencies: HookDependency[];
+};
+
+export type HookDependency = {
+  name: string;
+  type: "builtin" | "custom";
+  filePath?: string; // undefined for builtins
+};
+
+export type ComponentDefinition = {
+  name: string;
+  filePath: string;
+  consumes: HookDependency[];
+};
+
+export type ParseResult = {
+  hooks: HookDefinition[];
+  components: ComponentDefinition[];
+};
+
+export type GraphNode = {
+  id: string;
+  name: string;
+  filePath: string;
+  type: "hook" | "component";
+};
+
+export type GraphEdge = {
+  from: string;
+  to: string;
+  data: string[]; // Which data is exposed from a hook to a component
+  type: "depends-on" | "consumes";
+};
+
+export type Graph = {
+  nodes: GraphNode[];
+  edges: GraphEdge[];
+};
